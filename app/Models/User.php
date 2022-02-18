d4d41875571f22eb496c4da467f25534fcd6d793
Fahmi Amiruddin Nafi <amirfahmi8@gmail.com>
2022-02-18 08:34:56 +0700

    initial commit

---
+++ app/Models/User.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'type',
+        'credit'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    protected $appends = [
+        'type_description'
+    ];
+
+    public function getTypeDescriptionAttribute()
+    {
+        if ($this->type == 0) {
+            return "Regular User";
+        } elseif ($this->type == 1) {
+            return "Premium User";
+        } else {
+            return "Owner";
+        }
+    }
+
+}
